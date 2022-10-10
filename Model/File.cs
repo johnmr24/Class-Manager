063cc94e5b0c00cc706eed4247568aa6a344288d
Mann Patel <patelmann1939345060@gmail.com>
2022-10-10 17:08:40 -0400

    Synced model from john-UI branch

---
+++ Model/File.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Manager.Model
+{
+    internal class File
+    {
+        private string name;
+        private string path;
+
+        public File()
+        {
+            name = "";
+            path = "";
+        }
+
+        public File(string name, string path)
+        {
+            this.name = name;
+            this.path = path;
+        }
+    }
+}
