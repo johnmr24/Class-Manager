37693700240b696551821f086eb8c80aa8bb6ec5
daniellekj <danielle.kj056@gmail.com>
2022-11-29 19:10:11 -0500

    Pictures now change on page change

--- Forms/TutorialUI.cs
+++ Forms/TutorialUI.cs
@@ -12,15 +12,18 @@ using Class_Manager.Model;
 using Class_Manager.Properties;
 using Windows.Security.Authentication.Identity.Provider;
 using static System.Net.Mime.MediaTypeNames;
+using Image = System.Drawing.Image;
 
 namespace Class_Manager.Forms 
 {
     //Pagenum int for tracking the page you're on, plus a List with the string type (TutorialText). These will be used to track your current page and display the appropriate text.
+    //Another List of the type Image will be used to display the appropriate image.
     public partial class TutorialUIFrm : Form
     {   
         private readonly User user;
         public int pagenum = 0;
-        public List<string> TutorialText = new(); 
+        public List<string> TutorialText = new();
+        public List<Image> TutorialImages = new();
         public TutorialUIFrm(User u)
         {
             InitializeComponent(); 
@@ -29,7 +32,13 @@ namespace Class_Manager.Forms
             TutorialText.Add("After adding a class, you can click it and press the Assignments button to add an assignment. Enter the name of the assignment, it's due date, and anything you would like to note about said assignment. It will appear in the assignments box when you have the appropriate class selected. You can double click an assignment to change it's due date, notes, or name at any time.");
             TutorialText.Add("You can associate a file/file path with an assignment if you need to. Select the appropriate class, and then the assignment you want to add a file to. Press the File button to be prompted with a dialog to enter the file path. In this dialog, you can either open a second dialog to manually navigate to your file, or manually enter the file path in the text box.");
             TutorialText.Add("This concludes the tutorial for Class Manager. Remember, you can double click any class, assignment, or file to edit or remove them. When you double click on anything, a prompt will appear with fields for changing names, filepaths, due dates, etc. However, there's also a button to delete the double-clicked item when needed. You can also manually edit the XML loaded at startup by navigating to the Documents folder and finding the Class Manager folder. This is ONLY recommended to advanced users with XML knowledge, or anyone that needs to reset their Class Manager startup by deleting it.");
- 
+
+            TutorialImages.Add(Resources.Demo1);
+            TutorialImages.Add(Resources.Demo2);
+            TutorialImages.Add(Resources.Demo4);
+            TutorialImages.Add(Resources.Demo5);
+            TutorialImages.Add(Resources.Demo6);
+            
             user = u;
             //Make sure that the Prev button is visible.
             if (pagenum == 0) 
@@ -40,8 +49,10 @@ namespace Class_Manager.Forms
 
         private void NextBttn_Click(object sender, EventArgs e)
         {
-            //Sets the initial text in the TutorialBox.
+            //Sets the initial text in the TutorialBox and initial picture in TutorialPicture.
             TutorialBox.Text = TutorialText[pagenum];
+            TutorialPicture.Image = Resources.Demo1;
+            
             // After checking that you're on the next-to-last page, your next click will cause the Next button to disappear.
             if (pagenum >= 3) 
             {
@@ -49,14 +60,17 @@ namespace Class_Manager.Forms
                 NextBttn.Visible = false;
                 PrevBttn.Visible = true;  
                 TutorialBox.Text = TutorialText[pagenum];
+                TutorialPicture.Image = TutorialImages[pagenum];
             }
             //Normal page advancement, change text to current TutorialText index using pagenum.
+            //Change picture to current TutorialImages index using pagenum.
             else
             {
                 NextBttn.Visible = true; 
                 PrevBttn.Visible = true;
                 pagenum++; 
                 TutorialBox.Text = TutorialText[pagenum];
+                TutorialPicture.Image = TutorialImages[pagenum];
             }
         }
         private void PrevBttn_Click(object sender, EventArgs e)
@@ -69,8 +83,10 @@ namespace Class_Manager.Forms
                 NextBttn.Visible = true;
                 NextBttn.Text = "Next";
                 TutorialBox.Text = TutorialText[pagenum];
+                TutorialPicture.Image = TutorialImages[pagenum];
             }
             //Normal page backtrack, change text to current TutorialText index using pagenum.
+            //Change picture to current TutorialImages index using pagenum.
             else
             {
                 pagenum--; 
@@ -78,6 +94,7 @@ namespace Class_Manager.Forms
                 NextBttn.Visible = true;
                 NextBttn.Text = "Next";
                 TutorialBox.Text = TutorialText[pagenum];
+                TutorialPicture.Image = TutorialImages[pagenum];
             }
         }
         //For appearance changing.
